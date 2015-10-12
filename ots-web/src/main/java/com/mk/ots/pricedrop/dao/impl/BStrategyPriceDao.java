e10f8f0b6223415a2253fe690aa9c29c8e0c3eba
zhangyajun <yajun.zhang@imike.com>
2015-10-12 17:08:26 +0800

    导入包

--- ots-web/src/main/java/com/mk/ots/pricedrop/dao/impl/BStrategyPriceDao.java
+++ ots-web/src/main/java/com/mk/ots/pricedrop/dao/impl/BStrategyPriceDao.java
@@ -2,12 +2,13 @@ package com.mk.ots.pricedrop.dao.impl;
 
 import com.mk.framework.datasource.dao.mybatis.MyBatisDaoImpl;
 import com.mk.ots.pricedrop.dao.IBStrategyPriceDao;
+import com.mk.ots.pricedrop.model.BStrategyPrice;
 import com.mk.ots.promo.model.BPromotion;
 
 public class BStrategyPriceDao extends MyBatisDaoImpl<BPromotion, Long> implements IBStrategyPriceDao{
 
 	@Override
-	public void insert(com.mk.ots.pricedrop.model.BStrategyPrice bStrategyPrice) {
+	public void insert(BStrategyPrice bStrategyPrice) {
 		// TODO Auto-generated method stub
 		
 	}
