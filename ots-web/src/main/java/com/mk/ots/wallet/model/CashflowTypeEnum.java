ed91e3a73fb79b0275c8ed7804ad374818ef6fe5
huangjie <jie.huang@imike.com>
2015-10-13 21:34:32 +0800

    add

--- ots-web/src/main/java/com/mk/ots/wallet/model/CashflowTypeEnum.java
+++ ots-web/src/main/java/com/mk/ots/wallet/model/CashflowTypeEnum.java
@@ -10,7 +10,9 @@ public enum CashflowTypeEnum {
     CASHBACK_HOTEL_IN(2),           // 酒店点评返现
     CONSUME_ORDER_OUT_LOCK(3),      //订单消费已冻结
     CONSUME_ORDER_OUT_CONFIRM(4),   //订单消费已确认
-    CONSUME_ORDER_REFUND(5);        //订单消费退回
+    CONSUME_ORDER_REFUND(5),        //订单消费退回
+
+    CASHBACK_CARD_IN(6);           // 储值卡充值
 
     private int id;
 
