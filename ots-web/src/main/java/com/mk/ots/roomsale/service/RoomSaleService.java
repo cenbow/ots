50ff408e5f8d6bdcec9dacd00c162fb6c905cc75
xiaolong.kang <xiaolong.kang@imike.com>
2015-10-20 20:48:39 +0800

    add queryRoomPromoByHotel

--- ots-web/src/main/java/com/mk/ots/roomsale/service/RoomSaleService.java
+++ ots-web/src/main/java/com/mk/ots/roomsale/service/RoomSaleService.java
@@ -1,8 +1,6 @@
 package com.mk.ots.roomsale.service;
 
-import com.mk.ots.roomsale.model.TRoomSale;
-import com.mk.ots.roomsale.model.TRoomSaleConfig;
-import com.mk.ots.roomsale.model.TRoomSaleToIndex;
+import com.mk.ots.roomsale.model.*;
 
 import java.util.List;
 import java.util.Map;
@@ -46,9 +44,9 @@ public interface RoomSaleService {
 	 * @return
 	 * @throws Exception
 	 */
-	public List<Map<String, Object>> queryRoomPromoByHotel(String hotelId) throws Exception;
+	public List<RoomPromoDto> queryRoomPromoByHotel(TRoomSaleConfig bean);
 
 	public List<Map<String, Object>> queryRoomPromoInfoByHotel(String hotelId) throws Exception;
 	public Boolean checkRoomSale(TRoomSaleConfig bean);
-	public List<TRoomSaleToIndex> getUpdateIndexList(TRoomSaleConfig bean);
+	public List<RoomSaleToIndexDto> getUpdateIndexList(TRoomSaleConfig bean);
 }
