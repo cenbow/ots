26a6b82888b5a87676b80827b574296eec37472f
aarong <maomao.guan@imike.com>
2015-10-20 14:59:40 +0800

    add new methods

--- ots-web/src/main/java/com/mk/ots/mapper/RoomSaleMapper.java
+++ ots-web/src/main/java/com/mk/ots/mapper/RoomSaleMapper.java
@@ -1,15 +1,24 @@
 package com.mk.ots.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import com.mk.ots.room.sale.model.TRoomSale;
 
-import java.util.List;
 /**
  * RoomSaleMapper.
+ * 
  * @author kangxiaolong.
  *
  */
-public interface RoomSaleMapper{
-	public  List<TRoomSale> getSaleRoomListByHotel();
-	public  TRoomSale getOneRoomSale(TRoomSale bean);
-	public  List<TRoomSale> queryRoomSale(TRoomSale bean);
+public interface RoomSaleMapper {
+	public List<TRoomSale> getSaleRoomListByHotel();
+
+	public TRoomSale getOneRoomSale(TRoomSale bean);
+
+	public List<TRoomSale> queryRoomSale(TRoomSale bean);
+
+	public List<Map<String, Object>> queryRoomPromoByType(String roomTypeId);
+	
+	public List<Map<String, Object>> queryRoomPromoByHotel(String hotelId);
 }
