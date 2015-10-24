52ca7f179f8540fdc521f5baea892fdede1fa00f
bingqiu.yuan <bingqiu.yuan@imike.com>
2015-10-24 18:54:10 +0800

    Signed-off-by: bingqiu.yuan <bingqiu.yuan@imike.com>

---
+++ ots-web/src/main/resources/db/migration/mike3.0_430800.sql
@@ -0,0 +1,48 @@
+/** citycode 430800: 张家界 */
+delete from s_areainfo where citycode = '430800';
+        /****************************** 输出行政区SQL脚本: 开始 ***********************************/
+        insert into s_areainfo (areaid, areaname, pinyin, lat, lng, citycode, discode) values (3495,'永定区','yongdingqu',24.723962,116.732092,'430800','');
+        insert into s_areainfo (areaid, areaname, pinyin, lat, lng, citycode, discode) values (3497,'慈利县','cilixian',29.429999,111.139775,'430800','');
+        insert into s_areainfo (areaid, areaname, pinyin, lat, lng, citycode, discode) values (3496,'武陵源区','wulingyuanqu',29.34573,110.550434,'430800','');
+        insert into s_areainfo (areaid, areaname, pinyin, lat, lng, citycode, discode) values (3498,'桑植县','sangzhixian',29.400069,110.165819,'430800','');
+        /****************************** 输出行政区SQL脚本: 结束 ***********************************/
+delete from s_landmark where citycode = '430800';
+        /****************************** 输出景点SQL脚本: 开始 ***********************************/
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (17812,'慈利火车站','cilihuochezhan',2,29.417348,111.119059,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (17810,'慈利县中医院','cilixianzhongyiyuan',6,29.422106,111.121952,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (2194,'张家界航空工业职业技术学院','zhangjiajiehangkonggongyezhiyejishuxueyuan',7,29.147755,110.454038,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (10097,'张家界大峡谷','zhangjiajiedaxiagu',5,29.426895,110.69516,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (10096,'张家界万福温泉','zhangjiajiewanfuwenquan',5,29.414147,111.178123,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (10689,'张家界荷花机场','zhangjiajiehehuajichang',2,29.103857,110.451191,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (2196,'吉首大学张家界学院','jishoudaxuezhangjiajiexueyuan',7,29.151313,110.457252,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (17799,'慈利博物馆','cilibowuguan',5,29.430269,111.137581,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (17796,'慈利县儿童保健中心','cilixianertongbaojianzhongxin',6,29.425364,111.140602,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (10094,'宝峰湖景区','baofenghujingqu',5,29.329723,110.547254,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (10095,'张家界土家风情园','zhangjiajietujiafengqingyuan',5,29.115103,110.461304,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (10092,'黄龙洞','huanglongdong',5,29.374596,110.625626,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (17792,'慈利皮肤病医院','cilipifubingyiyuan',6,29.418977,111.12266,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (10093,'天门山','tianmenshan',5,29.032962,110.478686,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (17806,'人民广场','renminguangchang',5,29.123702,110.487051,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (15255,'蜗蜗居宾馆','wowojubinguan',5,29.418747,111.118401,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (17789,'环城医院','huanchengyiyuan',6,29.426034,111.141122,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (308,'张家界站','zhangjiajiezhan',2,29.105362,110.486734,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (17777,'桑植县汽车西站','sangzhixianqichexizhan',2,29.400532,110.153989,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (17780,'慈利县人民医院','cilixianrenminyiyuan',6,29.423207,111.130231,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (18258,'回龙观公园','huilongguangongyuan',5,29.122315,110.4936,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (17768,'民歌广场','mingeguangchang',5,29.396805,110.158904,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (18254,'梅尼百货','meinibaihuo',1,29.12439,110.486357,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (14127,'索溪峪','suoxiyu',5,29.346984,110.561526,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (14126,'黄石寨','huangshizhai',5,29.328873,110.432438,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (17770,'桑植县红十字医院','sangzhixianhongshiziyiyuan',6,29.393771,110.156956,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (14125,'金鞭溪','jinbianxi',5,29.321894,110.441557,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (14124,'袁家界','yuanjiajie',5,29.343058,110.443247,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (17772,'大众医院','dazhongyiyuan',6,29.131649,110.475069,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (18250,'张家界中医院','zhangjiajiezhongyiyuan',6,29.122219,110.485752,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (14123,'杨家界','yangjiajie',5,29.028912,110.704265,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (14122,'天子山','tianzishan',5,29.401558,110.443813,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (17775,'桑植县人民医院','sangzhixianrenminyiyuan',6,29.396369,110.146698,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (18249,'张家界市第二人民医院','zhangjiajieshidierrenminyiyuan',6,29.118336,110.546773,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (10414,'江垭温泉度假村','jiangyawenquandujiacun',5,29.500734,110.762311,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (17763,'桑植县妇幼保健院','sangzhixianfuyoubaojianyuan',6,29.40139,110.173661,'430800','');
+        insert into s_landmark (landmarkid, landmarkname, pinyin, ltype, lat, lng, citycode, discode) values (17764,'桑植县民族中医院','sangzhixianminzuzhongyiyuan',6,29.405514,110.180408,'430800','');
+        /****************************** 输出景点SQL脚本: 结束 ***********************************/
