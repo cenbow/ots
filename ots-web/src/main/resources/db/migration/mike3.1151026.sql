faaf129017094bb8aa97ccf155640e2d0054b952
huangjie <jie.huang@imike.com>
2015-10-26 16:00:51 +0800

    add mike3.1 sql,u_card

--- ots-web/src/main/resources/db/migration/mike3.1151026.sql
+++ ots-web/src/main/resources/db/migration/mike3.1151026.sql
@@ -89,3 +89,52 @@ CREATE TABLE `t_room_sale_type` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
+
+CREATE TABLE `b_card` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `batch_no` VARCHAR(50) NOT NULL COMMENT '批次号',
+  `name` VARCHAR(100) DEFAULT NULL COMMENT '名称',
+  `no` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(50) NOT NULL,
+  `status` TINYINT(4) NOT NULL DEFAULT '1' COMMENT '状态：1-生成 2-入库3-激活4-使用5-注销',
+  `price` DECIMAL(10,2) NOT NULL DEFAULT '0.00' COMMENT '面额',
+  `cost` DECIMAL(10,2) NOT NULL DEFAULT '0.00' COMMENT '售价',
+  `begin_date` DATETIME NOT NULL COMMENT '有效期开始日期',
+  `end_date` DATETIME NOT NULL COMMENT '有效期截止日期',
+  `description` TEXT COMMENT '描述',
+  `create_time` DATETIME DEFAULT NULL,
+  `update_time` DATETIME DEFAULT NULL,
+  `create_by` VARCHAR(50) DEFAULT NULL,
+  `update_by` VARCHAR(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `u_card_uselog` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `card_id` INT(11) NOT NULL COMMENT '卡id',
+  `mid` BIGINT(20) NOT NULL COMMENT '用户id',
+  `card_price` DECIMAL(10,2) NOT NULL DEFAULT '0.00' COMMENT '充值金额',
+  `create_time` DATETIME DEFAULT NULL COMMENT '使用时间',
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+
+##20151012 脚本 begin--
+alter table b_otaorder add promoType int(4) default 0 comment '特价类型, 0：非特价房间，1：今夜特价房间';
+alter table b_otaorder add roomTicket varchar(100) comment '房券代码';
+##20151012 脚本 end--
+
+##
+ALTER TABLE `u_member` ADD COLUMN `citycode`  varchar(50) NULL;
+
+##更新u_member历史数据
+update
+(
+select m.mid,min(o.id) oid from u_member m
+INNER JOIN b_otaorder o on m.mid = o.Mid
+where m.citycode is null and o.`OrderStatus` IN (180,190,200)
+GROUP BY m.mid
+) mm LEFT JOIN b_otaorder o on mm.mid = o.Mid and mm.oid = o.id
+left join t_hotel h on h.id = o.HotelId
+LEFT JOIN u_member m on m.mid = mm.mid
+set m.citycode = h.citycode;
