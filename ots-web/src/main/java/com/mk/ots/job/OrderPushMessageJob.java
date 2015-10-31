9bb72e380f7277db3d9f54b914316ac7e4639c51
huangjie <jie.huang@imike.com>
2015-10-31 11:59:56 +0800

    add job log

--- ots-web/src/main/java/com/mk/ots/job/OrderPushMessageJob.java
+++ ots-web/src/main/java/com/mk/ots/job/OrderPushMessageJob.java
@@ -2,6 +2,8 @@ package com.mk.ots.job;
 
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import com.mk.framework.AppUtils;
@@ -9,11 +11,16 @@ import com.mk.ots.order.service.OrderServiceImpl;
 
 public class OrderPushMessageJob  extends QuartzJobBean{
 	private OrderServiceImpl orderService = AppUtils.getBean(OrderServiceImpl.class);
+	private static Logger logger = LoggerFactory.getLogger(OrderPushMessageJob.class);
 	@Override
 	protected void executeInternal(JobExecutionContext context)
 			throws JobExecutionException {
+		logger.info("订单 push 消息job启动。。。");
 		orderService.pushMessage();
+		logger.info("订单 push 消息job完成");
+		logger.info("订单 push PMS2 消息job启动。。。");
 		orderService.pushMessagePms2Order();
+		logger.info("订单 push PMS2 消息job完成");
 	}
 
 }
