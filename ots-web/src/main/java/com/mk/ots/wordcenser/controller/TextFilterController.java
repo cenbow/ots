fca4ccb19785bcb0d09486fb104d0d4c55685c5c
kirin.li <kirin.li@dianping.com>
2015-11-10 11:47:36 +0800

    fix

--- ots-web/src/main/java/com/mk/ots/wordcenser/controller/TextFilterController.java
+++ ots-web/src/main/java/com/mk/ots/wordcenser/controller/TextFilterController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @Controller
@@ -28,4 +29,14 @@ public class TextFilterController {
         return new ResponseEntity<Map<String, Object>>(rtnMap, HttpStatus.OK);
     }
 
+    @RequestMapping("/test")
+    public ResponseEntity<Map<String, Object>> test(HttpServletRequest request, String token) {
+        String text = request.getParameter("text");
+        System.out.printf("校验结果 " + textFilterService.checkSensitiveWord(text));
+        Map<String, Object> rtnMap = Maps.newHashMap();
+        rtnMap.put("success", true);
+        rtnMap.put("校验结果", textFilterService.checkSensitiveWord(text));
+        rtnMap.put("message", "重新加载敏感词成功");
+        return new ResponseEntity<Map<String, Object>>(rtnMap, HttpStatus.OK);
+    }
 }
