e966084e80938dd5e3c587a9e4c9f9af243c4393
aarong <maomao.guan@imike.com>
2015-11-10 20:16:06 +0800

    add enum type for saletypeid

---
+++ ots-web/src/main/java/com/mk/ots/common/enums/HotelPromoEnum.java
@@ -0,0 +1,28 @@
+package com.mk.ots.common.enums;
+
+/**
+ * all promotion types used for queries. 
+ * <p>
+ * has equivalent value with t_room_sale_type.id
+ * 
+ * @author AaronG
+ *
+ */
+public enum HotelPromoEnum {
+    Night("1","今夜特价"),OneDollar("6","一元特价"), Day("11", "白天特价"), Theme("16", "主题房");
+    private String code;
+    private String text;
+
+    HotelPromoEnum(String code, String text) {
+        this.code = code;
+        this.text = text;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
