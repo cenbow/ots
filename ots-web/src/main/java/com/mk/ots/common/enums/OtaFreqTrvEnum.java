220045d0e9911db5d8f6e0a5b8a5b4069e57c030
huangjie <jie.huang@imike.com>
2015-11-26 12:43:08 +0800

    fix checkUserAdders

--- ots-web/src/main/java/com/mk/ots/common/enums/OtaFreqTrvEnum.java
+++ ots-web/src/main/java/com/mk/ots/common/enums/OtaFreqTrvEnum.java
@@ -16,6 +16,7 @@ public enum OtaFreqTrvEnum {
 	CARD_ID_IS_NOT_PMS_SCAN("41","身份证号不为PMS扫描"),
 	CARD_ID_NOT_FIRST("50","身份证号不是第一次使用"),
 	ZHIFU_NOT_FIRST("70","支付账号不是第一次使用"),
+//	LOCATION_NULL("80","用户未允许眯客获取其下单时坐标，无法判断用户位置"),
 	OUT_OF_RANG("90","下单位置不在酒店1公里范围内"),
 	CHECKIN_LESS4("100","入住30分钟内"),
 	OVER_RANG("110","结算超出定额"),
