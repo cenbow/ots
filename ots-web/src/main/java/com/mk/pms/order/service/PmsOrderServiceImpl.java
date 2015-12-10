4f4b5141fc4ca0fa8707a5d68bb8f63ac5eeee79
aarong <maomao.guan@imike.com>
2015-12-10 23:46:12 +0800

    downgrade transaction scope

--- ots-web/src/main/java/com/mk/pms/order/service/PmsOrderServiceImpl.java
+++ ots-web/src/main/java/com/mk/pms/order/service/PmsOrderServiceImpl.java
@@ -18,6 +18,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
@@ -305,6 +307,7 @@ public class PmsOrderServiceImpl implements PmsOrderService {
 	 * @return
 	 */
 	@SuppressWarnings("rawtypes")
+	@Transactional(readOnly = false, propagation = Propagation.SUPPORTS) 		
 	@Override
 	public Map saveCustomerNo(Map param) {
 		PmsOrderServiceImpl.logger.info("OTSMessage::PmsOrderServiceImpl::saveCustomerNo::参数::" + param);
@@ -569,7 +572,8 @@ public class PmsOrderServiceImpl implements PmsOrderService {
 		order.saveOrUpdate();
 		return order;
 	}
-
+	
+	@Transactional(readOnly = false, propagation = Propagation.SUPPORTS) 	
 	public void ChangeOtaOrder(List<PmsRoomOrder> pmsRoomOrderList, Map unlockParam) {
 		PmsOrderServiceImpl.logger.info("OTSMessage::roomOrderList::换房::{}", pmsRoomOrderList);
 		if (pmsRoomOrderList == null) {
