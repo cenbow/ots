7fb23850bc8b7db3b6f1fe0fa8bd535a23f6b0eb
chenqi <qi.chen@imke.com>
2015-12-23 20:06:18 +0800

    #r fix

--- ots-web/src/main/java/com/mk/ots/order/service/OrderServiceImpl.java
+++ ots-web/src/main/java/com/mk/ots/order/service/OrderServiceImpl.java
@@ -2948,7 +2948,7 @@ public class OrderServiceImpl implements OrderService {
 		}
         //设备号对应手机号超过三个手机号码 刷单校验
         OtaFreqTrvEnum otaFreqTrvEnum = qiekeRuleService.checkSysNo(otaorder, 3, OtaFreqTrvEnum.DEVICE_NUM_OVER);
-        if(otaFreqTrvEnum == null || OtaFreqTrvEnum.L1.getId().equals(otaFreqTrvEnum.getId())){
+        if(otaFreqTrvEnum == null || OtaFreqTrvEnum.DEVICE_NUM_OVER.getId().equals(otaFreqTrvEnum.getId())){
             otaorder.setSpreadUser(otaorder.getHotelId());
             otaorder.set("Invalidreason", OtaFreqTrvEnum.DEVICE_NUM_OVER.getId());
         }
