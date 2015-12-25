aa29a2f10a66f2361dcf5a53b36d0204453a5e0d
jinxin <jinxin.liu@imike.com>
2015-12-25 14:30:33 +0800

    Signed-off-by: jinxin <jinxin.liu@imike.com>

--- ots-web/src/main/java/com/mk/ots/view/controller/SyViewLogController.java
+++ ots-web/src/main/java/com/mk/ots/view/controller/SyViewLogController.java
@@ -54,6 +54,8 @@ public class SyViewLogController {
                 resultrtnMap.put("errmsg", "获取目标data失败.");
                 return new ResponseEntity<Map<String, Object>>(resultrtnMap, HttpStatus.OK);
             }
+            res = res.replace("\\","");
+            logger.info("【sys/addviewevent】 res  aftrer  replace  is : {}", res);
             JSONObject  jsObject = JSONObject.parseObject(res);
              String  dateStr =  jsObject.getString("data");
             logger.info("【sys/addviewevent】 dataStr is : {}", dateStr);
