7c025c4ec6af9645634be759b22f16a9d608b1cb
chenqi <qi.chen@imke.com>
2015-12-29 15:08:01 +0800

    #r fix run job week

--- ots-web/src/main/java/com/mk/ots/job/weekClearingJob.java
+++ ots-web/src/main/java/com/mk/ots/job/weekClearingJob.java
@@ -2,11 +2,14 @@ package com.mk.ots.job;
 
 import com.mk.framework.AppUtils;
 import com.mk.ots.bill.service.BillOrderService;
+import com.mk.ots.common.utils.DateUtils;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.util.Date;
 
 public class weekClearingJob extends QuartzJobBean {
     private static Logger logger = LoggerFactory.getLogger(weekClearingJob.class);
@@ -15,7 +18,8 @@ public class weekClearingJob extends QuartzJobBean {
             throws JobExecutionException {
         logger.info("周结算开始");
         BillOrderService billOrderDAO = AppUtils.getBean(BillOrderService.class);
-        billOrderDAO.runtWeekClearing(null,null);
+        Date beginTime = DateUtils.getDateFromString(DateUtils.formatDateTime(new Date(), DateUtils.FORMAT_DATE), DateUtils.FORMAT_DATE);
+        billOrderDAO.runtWeekClearing(beginTime,null);
         logger.info("周结算完成");
 
     }
