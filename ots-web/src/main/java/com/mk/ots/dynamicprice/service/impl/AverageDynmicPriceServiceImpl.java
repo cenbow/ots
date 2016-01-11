abe974fc5aa32361fe393234e944ac84096764be
kirin.li <kirin.li@dianping.com>
2016-01-11 22:21:41 +0800

    fix

--- ots-web/src/main/java/com/mk/ots/dynamicprice/service/impl/AverageDynmicPriceServiceImpl.java
+++ ots-web/src/main/java/com/mk/ots/dynamicprice/service/impl/AverageDynmicPriceServiceImpl.java
@@ -1,6 +1,7 @@
 package com.mk.ots.dynamicprice.service.impl;
 
 import com.mk.ots.dynamicprice.service.AverageDynamicPriceService;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.Random;
@@ -8,6 +9,7 @@ import java.util.Random;
 /**
  * Created by kirinli on 16/1/11.
  */
+@Service
 public class AverageDynmicPriceServiceImpl implements AverageDynamicPriceService {
     @Override
     public BigDecimal getRoomTypeAverageDynamicPrice(String hotelId, String roomTypeId, Integer checkInOClock) {
--- ots-web/src/main/java/com/mk/ots/dynamicprice/service/impl/MinDynamicPriceServiceImpl.java
+++ ots-web/src/main/java/com/mk/ots/dynamicprice/service/impl/MinDynamicPriceServiceImpl.java
@@ -1,6 +1,7 @@
 package com.mk.ots.dynamicprice.service.impl;
 
 import com.mk.ots.dynamicprice.service.MinDynamicPriceService;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.Random;
@@ -8,6 +9,7 @@ import java.util.Random;
 /**
  * Created by kirinli on 16/1/11.
  */
+@Service
 public class MinDynamicPriceServiceImpl implements MinDynamicPriceService{
     @Override
     public BigDecimal getHotelMinDynamicPrice(String hotelId, String roomTypeId, Integer checkInOCloc) {
